04151e8da2a277d23579f0e8edcbba274acc65ed
Nama Anda <emailpribadi@example.com>
2025-11-19 19:46:13 +0700

    Commit No. 1: Implement ViewModel to Handle Rotation

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.lab_week_10
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: TotalViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Inisialisasi ViewModel
+        viewModel = ViewModelProvider(this)[TotalViewModel::class.java]
+
+        // Tampilkan nilai awal
+        updateText(viewModel.total)
+
+        // Setup button
+        prepareViewModel()
+    }
+
+    private fun updateText(total: Int) {
+        findViewById<TextView>(R.id.text_total).text =
+            getString(R.string.text_total, total)
+    }
+
+    private fun prepareViewModel() {
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            val newTotal = viewModel.incrementTotal()
+            updateText(newTotal)
         }
     }
 }
